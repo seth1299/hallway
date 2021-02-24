b2e82275bd13c40ba1a11d44f2baa3b846485623
seth1299 <seth.grimes1999@gmail.com>
2021-02-24 16:57:01 -0500

    Initial commit with main menu stuff
    Description text

---
+++ GP4 Hallway With Guard/Assets/Scripts/UI or UX Scripts/MainMenuController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    // "menuSelector" is an int that selects which menu the player is on.
+    private int menuSelector;
+
+
+
+    // "mainMenuCanvas" is the Canvas for the Main Menu. "aboutMenuCanvas" is the Canvas for the About Menu inside of the Main Menu.
+    public Canvas mainMenuCanvas, aboutMenuCanvas;
+
+    void Start()
+    {
+        menuSelector = 0;
+    }
+
+    // The StartGame() function loads the "Game" scene, which should be the actual game content.
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    // The About() function loads the "About" scene, which should contain the instructions for how to play the game.
+    public void About()
+    {
+        menuSelector = 1;
+        //SceneManager.LoadScene("About");
+    }
+
+    // The MainMenu() function returns the user to the main menu.
+    public void MainMenu()
+    {
+        menuSelector = 0;
+        //SceneManager.LoadScene("MainMenu");
+    }
+
+    // This quits the game when the "exit" button is clicked.
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    void Update()
+    {
+        // This code simply closes the application when the "ESC" key is pressed, as required in the "requirements for ALL games" section on Webcourses.
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+
+        if (menuSelector == 1)
+        {
+            mainMenuCanvas.enabled = false;
+            aboutMenuCanvas.enabled = true;
+        }
+            
+        else
+        {
+            mainMenuCanvas.enabled = true;
+            aboutMenuCanvas.enabled = false;
+        }
+    }
+
+    public int GetMenuSelector()
+    {
+        return menuSelector;
+    }
+
+}
---
+++ GP4 Hallway With Guard/Assets/Scripts/UI or UX Scripts/PauseMenuController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour
+{
+
+    // "canvas" is the Canvas object that you want to control. In this case, it's the entire "Pause Menu" canvas.
+    public Canvas canvas;
+
+    // "isPaused" keeps track of if the game is paused or not. "true" if the game is paused, otherwise it is "false".
+    private bool isPaused = false;
+
+    // "about" keeps track of if the player is on the "about" screen or not. "true" if the player is on the pause screen, or "false" otherwise.
+    private bool about = false;
+
+    // Start() is called once at the Scene load.
+    void Start()
+    {
+        // This just sets the "Pause" menu to be invisible when the game starts.
+        canvas.enabled = false;
+    }
+
+    // Update is called once per frame.
+    void Update()
+    {
+        // This sets paused to true if it is currently false or vice versa. It also recognizes if the player is in the about menu and closes that out (only if the
+        // game is already paused)
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            isPaused = !isPaused;
+            if ( about )
+                about = false;
+        }
+
+        // This makes the pause menu "appear" and "disappear" at the appropriate times (when paused or the about menu is clicked)
+        if ( isPaused )
+            canvas.enabled = true;
+        else
+            canvas.enabled = false;
+        if ( about )
+            canvas.enabled = false;
+
+        // This sets the time scale to 0 if the game is paused, otherwise sets it to 1. Meaning time doesn't move when paused and it resumes again when unpaused.
+        if ( isPaused )
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
+    }
+
+    // AboutMenu() handles "changing" the "pause" menu to the "about" menu in-game (not in the main menu).
+    public void AboutMenu()
+    {
+        about = true;
+    }
+
+    // GetIsPaused() returns true if the game is paused, otherwise it returns false.
+    public bool GetIsPaused()
+    {
+        return isPaused;
+    }
+
+    // GetIsInAboutMenu() returns true if the player is in the About, otherwise it returns false.
+    public bool GetIsInAboutMenu()
+    {
+        return about;
+    }
+}
