bcb31dd1744ce2775995fa60d54f3a57108597ef
Jess <jwise1596@gmail.com>
2021-03-04 17:10:47 -0500

    IDK

---
+++ JessBranch/Assets/Scripts/UI or UX Scripts/MusicController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    public AudioSource backgroundMusic;
+    public AudioSource lossMusic;
+    public AudioSource victoryMusic;
+
+    void Start()
+    {
+        backgroundMusic.Play();
+    }
+
+    public void playSound(bool victory)
+    {
+        backgroundMusic.Stop();
+        if (victory == true)
+        {
+            victoryMusic.Play();
+        }
+        else
+        {
+            Debug.Log("Playing loss music...");
+            lossMusic.Play();
+        }
+    }
+
+}
